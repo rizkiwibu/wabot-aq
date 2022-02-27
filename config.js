dfbc1f161fb4b499b73f94f06b60e0a08593791a
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-02-27 16:46:52 +0700

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['6283817161861', '6283817161861', '6283817161861'] // Put your number here
+global.mods = ['6283817161861'] // Want some help?
+global.prems = ['6283817161861'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
